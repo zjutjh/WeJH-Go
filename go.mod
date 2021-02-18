fff7fa7970b509b3c4a87f55d91aea67ab52466b
Node_Sans <lixiao189@outlook.com>
2021-02-18 09:38:05 +0800

    Update some thing

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "src"
+module src
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/hello", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "hello",
+        })
+    })
+    r.Run()
+}
