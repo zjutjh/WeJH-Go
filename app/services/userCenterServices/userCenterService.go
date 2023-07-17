fc70aef117900a9d2087699fe1e218c817ef19c3
cbluebird <crk2778355976@163.com>
2023-07-17 17:46:12 +0800

    新用户中心功能上线

---
+++ app/services/userCenterServices/userCenterService.go
@@ -0,0 +1,49 @@
+package userCenterServices
+
+import (
+	"encoding/json"
+	"fmt"
+	"wejh-go/app/apiException"
+	"wejh-go/app/utils/fetch"
+	"wejh-go/config/api/userCenterApi"
+)
+
+type UserCenterResponse struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func FetchHandleOfPost(form map[string]string, url userCenterApi.UserCenterApi) (*UserCenterResponse, error) {
+	f := fetch.Fetch{}
+	f.Init()
+	res, err := f.PostJsonForm(userCenterApi.UserCenterHost+string(url), form)
+	if err != nil {
+		fmt.Println(err)
+		return nil, apiException.RequestError
+	}
+	rc := UserCenterResponse{}
+	err = json.Unmarshal(res, &rc)
+	if err != nil {
+		fmt.Println(err)
+		return nil, apiException.RequestError
+	}
+	return &rc, nil
+}
+
+func FetchHandleOfGet(url userCenterApi.UserCenterApi) (*UserCenterResponse, error) {
+	f := fetch.Fetch{}
+	f.Init()
+	res, err := f.Get(userCenterApi.UserCenterHost + string(url))
+	if err != nil {
+		fmt.Println(err)
+		return nil, apiException.RequestError
+	}
+	rc := UserCenterResponse{}
+	err = json.Unmarshal(res, &rc)
+	if err != nil {
+		fmt.Println(err)
+		return nil, apiException.RequestError
+	}
+	return &rc, nil
+}
